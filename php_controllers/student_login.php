5a8c371c359ee3f9c67b8104b4aea957fe3d5673
jatinj615 <jatinj615@gmail.com>
2017-08-29 15:41:35 +0530

    3 hr timer in student portal

--- php_controllers/student_login.php
+++ php_controllers/student_login.php
@@ -5,14 +5,22 @@
 	include 'database_connection.php';
 	$connection = new DatabaseConnect();
 	$con = $connection->connect();
-	$result = mysqli_query($con , 'Select STU_FIRST_NAME,STU_ID,STU_SEM,STU_DEPARTMENT from STUDENTS where STU_ID="'.$stu_id.'" and STU_PASSWORD= "'.$stu_pass.'"');
-	if($result && mysqli_num_rows($result) > 0){
-		$row = mysqli_fetch_assoc($result);
-		$_SESSION['stu_first_name'] = $row['STU_FIRST_NAME'];
-		$_SESSION['stu_id'] = $row['STU_ID'];
-		$_SESSION['stu_sem'] = $row['STU_SEM'];
-		$_SESSION['stu_department'] = $row['STU_DEPARTMENT'];
-		header("Location: /Online_examination/student_profile.php");
+	//Inserting login and logout time
+	$add_login_time_query = 'Update STUDENTS set LOGIN_AT=now(),LOGOUT_AT=ADDTIME(now(), "03:00:00") where STU_ID="'.$stu_id.'" and STU_PASSWORD="'.$stu_pass.'"';
+	$selection_query =  'Select STU_FIRST_NAME,STU_ID,STU_SEM,STU_DEPARTMENT,LOGOUT_AT from STUDENTS where STU_ID="'.$stu_id.'" and STU_PASSWORD= "'.$stu_pass.'"';
+	$login_time = mysqli_query($con , $add_login_time_query);
+	//Create sessions for furthur info
+	if ($login_time) {
+		$result = mysqli_query($con , $selection_query);
+		if($result && mysqli_num_rows($result) > 0){
+			$row = mysqli_fetch_assoc($result);
+			$_SESSION['stu_first_name'] = $row['STU_FIRST_NAME'];
+			$_SESSION['stu_id'] = $row['STU_ID'];
+			$_SESSION['logout_time'] = $row['LOGOUT_AT'];
+			$_SESSION['stu_sem'] = $row['STU_SEM'];
+			$_SESSION['stu_department'] = $row['STU_DEPARTMENT'];
+			header("Location: /Online_examination/student_profile.php");
+		}	
 	}else {
 		$error = 'error';
 		header("Location: /Online_examination/index.php?error=$error");
--- question_paper.php
+++ question_paper.php
@@ -19,9 +19,36 @@
 <html lang="en">
 <head>
 	<meta charset="UTF-8">
-	<title>Online Examination</title>
+	<title>Online Examination</title> 
+	<script type="text/javascript">
+ 		var countDownDate = new Date("<?php echo $_SESSION['logout_time'] ?>").getTime();
+ 		var x = setInterval(function() {
+
+		    // Get todays date and time
+		    var now = new Date().getTime();
+		    
+		    // Find the distance between now an the count down date
+		    var distance = countDownDate - now;
+		    
+		    // Time calculations for days, hours, minutes and seconds
+		    var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+		    var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+		    var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+		    
+		    // Output the result in an element with id="demo"
+		    document.getElementById("demo").innerHTML =  hours + "h "
+		    + minutes + "m " + seconds + "s ";
+		    
+		    // If the count down is over, write some text 
+		    if (distance < 0) {
+		        clearInterval(x);
+		        document.getElementById("demo").innerHTML = "Time Up";
+		    }
+		}, 1000);
+ 	</script>
 </head>
 <body>
+<p id="demo"></p>
 <div>
 	<ul>
  	<?php 
